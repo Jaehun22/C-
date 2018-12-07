00780fb6336767bb2bb64853887d5af9563b2716
Jo jae hun <wognskec@naver.com>
2018-12-07 16:56:46 +0900

    14 주차 연습

--- week14/test1.c
+++ week14/test1.c
@@ -6,18 +6,18 @@ void m_strcpy(char * str1,char *str2);
 int main(){
     char a[10],b[10];
     scanf("%s %s",a,b);
-    printf("첫번째 문자열은 : %s \n두번째 문자열은 : %s",a,b);
+    printf("첫번째 문자열은 : %s \n두번째 문자열은 : %s\n",a,b);
     
     m_strcpy(b,a);
     printf("복사하기\n");
-    printf("첫번째 문자열은 : %s \n두번째 문자열은 : %s",a,b);
+    printf("첫번째 문자열은 : %s \n두번째 문자열은 : %s\n",a,b);
 
     return 0;
 }
 
 void m_strcpy(char *str1,char *str2){
     while(*str2 != '\0'){
-        *str1 == *str2;
+        *str1 = *str2;
         str1++;
         str2++;
     }
